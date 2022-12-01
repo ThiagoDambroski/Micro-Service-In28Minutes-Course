63492e3ff3979f3a074fd5cf4615dd8a32ac2c25
ThiagoDambroski <dambroskic@hotmail.com>
2022-12-01 10:32:23 -0300

    Internationalization

--- restful-web-service-in28Minutes/src/main/java/com/dambroski/restfulwebservicein28Minutes/helloWorld/HelloWorldController.java
+++ restful-web-service-in28Minutes/src/main/java/com/dambroski/restfulwebservicein28Minutes/helloWorld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.dambroski.restfulwebservicein28Minutes.helloWorld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping("/hello-world")
 	public String helloWorld() {
@@ -20,5 +28,13 @@ public class HelloWorldController {
 	@GetMapping("/hello/{message}")
 	public HelloWorldBean helloMessage(@PathVariable("message") String message) {
 		return new HelloWorldBean("Hello World " +  message);
+	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+		
 	}
 }

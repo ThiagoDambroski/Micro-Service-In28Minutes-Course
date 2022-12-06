fcfbaed14b131f6ed18151e039dadb92cc1213a3
ThiagoDambroski <dambroskic@hotmail.com>
2022-12-05 21:45:28 -0300

    H2 dataBase

--- restful-web-service-in28Minutes/src/main/java/com/dambroski/restfulwebservicein28Minutes/user/User.java
+++ restful-web-service-in28Minutes/src/main/java/com/dambroski/restfulwebservicein28Minutes/user/User.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,8 +20,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity(name = "user_details")
 public class User {
 	
+	@Id
+	@GeneratedValue(generator = "user_id",strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@Size(min = 2,message = "name should have at least 2 characters")
---
+++ restful-web-service-in28Minutes/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user_details(id,birth_date,name)
+values(1001, current_date() ,'Leticia');
+
+insert into user_details(id,birth_date,name)
+values(1002, current_date() ,'Thiago');

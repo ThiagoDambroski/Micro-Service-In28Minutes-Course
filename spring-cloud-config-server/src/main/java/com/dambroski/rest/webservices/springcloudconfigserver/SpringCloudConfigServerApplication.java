cadde7b84bae902eb9d832548b39db1dbe509eda
ThiagoDambroski <dambroskic@hotmail.com>
2022-12-08 10:35:01 -0300

    local config repo

--- spring-cloud-config-server/src/main/java/com/dambroski/rest/webservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/dambroski/rest/webservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,10 @@ package com.dambroski.rest.webservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 

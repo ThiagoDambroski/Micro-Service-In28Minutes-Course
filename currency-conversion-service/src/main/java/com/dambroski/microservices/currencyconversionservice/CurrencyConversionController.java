3e00444e240ec4f981454bb344e9b10e56694cda
ThiagoDambroski <dambroskic@hotmail.com>
2022-12-12 10:13:02 -0300

    ivoking the currency-exchage service

--- currency-conversion-service/src/main/java/com/dambroski/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/dambroski/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,8 +1,12 @@
 package com.dambroski.microservices.currencyconversionservice;
 
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -10,7 +14,20 @@ public class CurrencyConversionController {
 	@GetMapping("/currency-conversion/from/{coin1}/to/{coin2}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable("quantity") double quantity,
 			@PathVariable("coin1") String coin1,@PathVariable("coin2")String coin2) {
-		CurrencyConversion conversion = new CurrencyConversion(10001L, coin1, coin2, quantity, 1.0, 1.0,"");
+		
+		HashMap<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("coin1",coin1);
+		uriVariables.put("coin2", coin2);
+		
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+				.getForEntity("http://localhost:8000/currencey-exchange/from/USD/to/INR",
+				CurrencyConversion.class,uriVariables);
+		
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		
+		CurrencyConversion conversion = new CurrencyConversion(currencyConversion.getId()
+				, coin1, coin2, quantity, currencyConversion.getConversionMultiple(),
+				(quantity * currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
 		return conversion;
 	}
 }

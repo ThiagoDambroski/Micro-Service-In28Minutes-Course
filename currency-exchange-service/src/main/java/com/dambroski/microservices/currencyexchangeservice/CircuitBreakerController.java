8de86b28d030bdbe8fe0b0d10f684039fa61f895
ThiagoDambroski <dambroskic@hotmail.com>
2023-01-03 12:28:39 -0300

    bulkhead

--- currency-exchange-service/src/main/java/com/dambroski/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/dambroski/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -17,7 +19,9 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@CircuitBreaker(name = "dafault",fallbackMethod = "hardcodedResponse")
+	//@CircuitBreaker(name = "dafault",fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name="default")
+	@Bulkhead(name = "default")
 	public String sampleApi() {
 		logger.info("Sample Api call Recive");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/test",String.class);
